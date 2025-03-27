d90c2b680cc2d38e4a23b1dc7eb3eb0ede2713d1
David Cui <112504777+boncui@users.noreply.github.com>
2025-03-26 21:53:14 -0500

    imported data

---
+++ model/model1.py
@@ -0,0 +1,9 @@
+# model1.py
+import pandas as pd
+import numpy as np
+
+#Path: /Users/boncui/Desktop/Projects/Personal Projects/brain-tumor/data/brain_tumor_dataset.csv
+data = pd.read_csv("../data/brain_tumor_dataset.csv")
+
+print(data.head())
+
